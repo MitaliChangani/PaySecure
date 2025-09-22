29656f0c4f8ed958a155ae4b2da6df019aa06f25
Jeels <jeelsherathiya25@gmail.com>
2025-09-22 16:50:50 +0530

    all

--- paysecure_frontend/src/App.jsx
+++ paysecure_frontend/src/App.jsx
@@ -3,43 +3,43 @@ import Register from "./Components/Register";
 import Login from './Components/Login'
 import Forgot from './Components/Forgot'
 import Otp from './Components/Otp'
-<<<<<<< HEAD
+
 import Footer from './Components/Footer'
 import Home from './Components/Home'
-=======
+
 import Header from "./Components/Header";
 import FranchiseDs from "./Components/FranchiseDs";
-<<<<<<< HEAD
+
 import UserDs from "./Components/UserDs";
-=======
->>>>>>> 21339c205bb42dbf0ebc8614d704a64bdd90bf45
->>>>>>> 9264378579995b65d0fa3d12e4695e2995f2f348
+
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-<<<<<<< HEAD
-        <Route path="/Home" element={<Home />} />
-=======
-        {/* Default route */}
-        <Route path="/" element={<Header/>} />
+        return (
+                <Router>
+                        <Header/>
+                        <Routes>
 
-        {/* Example: if you want to navigate using /register */}
->>>>>>> 21339c205bb42dbf0ebc8614d704a64bdd90bf45
-        <Route path="/Register" element={<Register />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Forgot" element={<Forgot />} />
-        <Route path="/Otp" element={<Otp />} />
-<<<<<<< HEAD
-        <Route path="/Footer" element={<Footer />} />
-            </Routes>
-=======
-        <Route path="/FranchiseDs" element={<FranchiseDs />} />
-        <Route path="/UserDs" element={<UserDs />} />
-      </Routes>
->>>>>>> 21339c205bb42dbf0ebc8614d704a64bdd90bf45
-    </Router>
+                                <Route path="/" element={<Home />} />
+
+                                {/* Default route */}
+                                {/* <Route path="/Header" element={<Header />} /> */}
+
+                                {/* Example: if you want to navigate using /register */}
+
+                                <Route path="/Register" element={<Register />} />
+                                <Route path="/Login" element={<Login />} />
+                                <Route path="/Forgot" element={<Forgot />} />
+                                <Route path="/Otp" element={<Otp />} />
+
+                                <Route path="/Footer" element={<Footer />} />
+                        
+
+                        <Route path="/FranchiseDs" element={<FranchiseDs />} />
+                        <Route path="/UserDs" element={<UserDs />} />
+                </Routes>
+                <Footer/>
+
+    </Router >
   );
 }
 
