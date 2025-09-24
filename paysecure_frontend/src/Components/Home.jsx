638877de75c3d5f38164412ae5d1350028d17a70
Jeels <jeelsherathiya25@gmail.com>
2025-09-24 17:31:58 +0530

    all

--- paysecure_frontend/src/Components/Home.jsx
+++ paysecure_frontend/src/Components/Home.jsx
@@ -32,11 +32,12 @@ function Home() {
       desc: "Advanced encryption keeps your transactions safe.",
     },
   ];
+
   return (
     <>
-      <div className="min-h-screen flex items-center justify-center bg-blue-100">
-        <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-10 py-8 grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-16 items-center">
-          <div className="flex justify-center md:justify-end order-1 md:order-2 mt-[-70px]">
+      <div className="bg-blue-100">
+        <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-10 pt-16 pb-8 grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-16 items-center">
+          <div className="flex justify-center md:justify-end order-1 md:order-2">
             <div className="relative w-full max-w-md sm:max-w-lg md:max-w-3xl aspect-video bg-gray-900 rounded-xl shadow-2xl overflow-hidden border-4 border-gray-800">
               <video
                 src={VideoFile}
@@ -49,7 +50,8 @@ function Home() {
               <div className="absolute -bottom-6 left-1/2 -translate-x-1/2 w-[110%] h-6 bg-gray-700 rounded-b-3xl shadow-lg"></div>
             </div>
           </div>
-          <div className="flex flex-col justify-center space-y-6 text-center md:text-left order-2 md:order-1 px-4 sm:px-6 md:px-0">
+          <div className="flex flex-col justify-center space-y-6 text-center order-2 md:order-1 px-4 sm:px-6 md:px-0">
+
             <h1 className="text-2xl sm:text-3xl md:text-5xl lg:text-6xl font-bold text-gray-900 leading-snug">
               Receive & Withdraw Payments Directly to your <br />
               Account @ <span className="text-blue-700">0% Transaction Fee</span>
@@ -63,18 +65,17 @@ function Home() {
             <div className="flex flex-wrap gap-4 justify-center md:justify-start">
               <Link
                 to="/Login"
-                className="px-6 py-2 rounded-lg text-white bg-blue-700 font-semibold hover:bg-blue-800 transition-colors duration-300 text-sm sm:text-base md:text-lg"
+                className="px-6 py-2 ml-54 rounded-lg text-white bg-blue-700 font-semibold hover:bg-blue-800 transition-colors duration-300 text-sm sm:text-base md:text-lg"
               >
                 Continue &gt;
               </Link>
-
             </div>
           </div>
         </div>
       </div>
       <section
         id="features"
-        className="bg-blue-100 py-16 px-6 sm:px-10 lg:px-20"
+        className="bg-white pt-8 pb-16 px-6 sm:px-10 lg:px-20"
       >
         <div className="max-w-7xl mx-auto text-center mb-12">
           <h2 className="text-3xl sm:text-4xl font-bold text-gray-900">
@@ -103,4 +104,5 @@ function Home() {
     </>
   );
 }
+
 export default Home;
